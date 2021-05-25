e2ab74a815f2f18132d18b158761badfa5759d8e
Hamid Reza Oujand <hmdrzaa11@gmail.com>
2021-05-25 16:56:41 +0430

    add DB connection and server setup

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 let express = require("express");
 require("dotenv").config({ path: ".env" });
+let mongoose = require("mongoose");
+
+/*Database Connection */
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB in the HOUSE!"))
+  .catch((er) => {
+    console.log(er);
+    process.exit(1);
+  });
 
 let app = express();
+
+/*API  */
 
 let PORT = process.env.PORT || 3000;
 
